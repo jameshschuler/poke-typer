2616386854c991e3ecde0df0c17a35d2639e2c18
jamesschuler <jameshschuler1@gmail.com>
2022-11-27 20:18:44 -0800

    initial pwa config and support

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,34 @@
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
-import { defineConfig } from 'vite';
-import WindiCSS from 'vite-plugin-windicss';
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import WindiCSS from "vite-plugin-windicss";
 
-export default defineConfig( {
+export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/styles/app.scss";`
-      }
-    }
+        additionalData: `@import "./src/styles/app.scss";`,
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve( __dirname, './src' ),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  plugins: [ vue(), WindiCSS() ]
-} )
+  plugins: [
+    vue(),
+    WindiCSS(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
