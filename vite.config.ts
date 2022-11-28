c467c8a1f64fe5d6f5860703f70aa200f324477c
jamesschuler <jameshschuler1@gmail.com>
2022-11-27 20:25:38 -0800

    more config

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,7 @@ export default defineConfig({
     vue(),
     WindiCSS(),
     VitePWA({
+      injectRegister: "auto",
       registerType: "autoUpdate",
       workbox: {
         clientsClaim: true,
@@ -28,6 +29,26 @@ export default defineConfig({
       },
       devOptions: {
         enabled: true,
+      },
+      includeAssets: ["apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "PokéTyper",
+        short_name: "PokéTyper",
+        description:
+          "Simple web app that displays the effectiveness of various Pokémon types compared to other types.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
